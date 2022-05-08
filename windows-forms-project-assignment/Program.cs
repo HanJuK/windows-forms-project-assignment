ccda3f76d198ee9e0038199798d1f72d18ab37e6
HanJuK <qwerty03081@gmail.com>
2022-05-08 22:52:15 +0900

    use Windows Forms

--- windows-forms-project-assignment/Program.cs
+++ windows-forms-project-assignment/Program.cs
@@ -2,11 +2,12 @@
 
 namespace windows_forms_project_assignment
 {
-    internal class Program
+    internal class Program : System.Windows.Forms.Form
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            System.Windows.Forms.Application.Run(new Program());
         }
     }
 }
