81e73e3f2636da15890179060ec445e569d791b0
HanJuK <qwerty03081@gmail.com>
2022-05-08 22:46:54 +0900

    init

---
+++ windows-forms-project-assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace windows_forms_project_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
