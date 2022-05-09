1d5643401c386dc698ee45af6471d0b020d6c7b5
HanJuK <qwerty03081@gmail.com>
2022-05-09 19:53:21 +0900

    add admin, order button to MainForm

---
+++ windows-forms-project-assignment/AdminForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace windows_forms_project_assignment
+{
+    internal class AdminForm : Form
+    {
+        /** define constants */
+        const string STRING_FORM_TITLE = "Self Ordering System - Admin Screen";
+
+        /** AdminForm class constructor */
+        public AdminForm()
+        {
+            /** set the title of the Form */
+            this.Text = STRING_FORM_TITLE;
+        }
+    }
+}
---
+++ windows-forms-project-assignment/OrderForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace windows_forms_project_assignment
+{
+    internal class OrderForm : Form
+    {
+        /** define constants */
+        const string STRING_FORM_TITLE = "Self Ordering System - Order Screen";
+
+        /** AdminForm class constructor */
+        public OrderForm()
+        {
+            /** set the title of the Form */
+            this.Text = STRING_FORM_TITLE;
+        }
+    }
+}
