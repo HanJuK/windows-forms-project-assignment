271730dd02031fe829081eb64ed0b49ada381563
HanJu Kim <48195650+HanJuK@users.noreply.github.com>
2022-05-12 15:53:41 +0900

    change textBox to numericUpDown for product price

--- windows-forms-project-assignment/CreateUpdateProductForm.cs
+++ windows-forms-project-assignment/CreateUpdateProductForm.cs
@@ -12,7 +12,7 @@ namespace windows_forms_project_assignment
         private Label labelProductName;
         private TextBox textBoxProductName;
         private Label labelProductPrice;
-        private TextBox textBoxProductPrice;
+        private NumericUpDown numericUpDownProductPrice;
         private Button buttonCreateUpdateProduct;
 
         /** CreateUpdateProductForm class constructor (initialize) */
@@ -29,8 +29,9 @@ namespace windows_forms_project_assignment
             this.labelProductName = new System.Windows.Forms.Label();
             this.textBoxProductName = new System.Windows.Forms.TextBox();
             this.labelProductPrice = new System.Windows.Forms.Label();
-            this.textBoxProductPrice = new System.Windows.Forms.TextBox();
             this.buttonCreateUpdateProduct = new System.Windows.Forms.Button();
+            this.numericUpDownProductPrice = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownProductPrice)).BeginInit();
             this.SuspendLayout();
             // 
             // labelProductName
@@ -38,7 +39,7 @@ namespace windows_forms_project_assignment
             this.labelProductName.AutoSize = true;
             this.labelProductName.Location = new System.Drawing.Point(12, 9);
             this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(92, 15);
+            this.labelProductName.Size = new System.Drawing.Size(115, 20);
             this.labelProductName.TabIndex = 0;
             this.labelProductName.Text = "Product Name: ";
             // 
@@ -46,7 +47,7 @@ namespace windows_forms_project_assignment
             // 
             this.textBoxProductName.Location = new System.Drawing.Point(12, 27);
             this.textBoxProductName.Name = "textBoxProductName";
-            this.textBoxProductName.Size = new System.Drawing.Size(100, 23);
+            this.textBoxProductName.Size = new System.Drawing.Size(100, 27);
             this.textBoxProductName.TabIndex = 2;
             // 
             // labelProductPrice
@@ -54,16 +55,9 @@ namespace windows_forms_project_assignment
             this.labelProductPrice.AutoSize = true;
             this.labelProductPrice.Location = new System.Drawing.Point(233, 9);
             this.labelProductPrice.Name = "labelProductPrice";
-            this.labelProductPrice.Size = new System.Drawing.Size(86, 15);
+            this.labelProductPrice.Size = new System.Drawing.Size(108, 20);
             this.labelProductPrice.TabIndex = 1;
             this.labelProductPrice.Text = "Product Price: ";
-            // 
-            // textBoxProductPrice
-            // 
-            this.textBoxProductPrice.Location = new System.Drawing.Point(172, 27);
-            this.textBoxProductPrice.Name = "textBoxProductPrice";
-            this.textBoxProductPrice.Size = new System.Drawing.Size(100, 23);
-            this.textBoxProductPrice.TabIndex = 3;
             // 
             // buttonCreateUpdateProduct
             // 
@@ -74,15 +68,33 @@ namespace windows_forms_project_assignment
             this.buttonCreateUpdateProduct.Text = "Create/Update Product";
             this.buttonCreateUpdateProduct.UseVisualStyleBackColor = true;
             // 
+            // numericUpDownProductPrice
+            // 
+            this.numericUpDownProductPrice.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDownProductPrice.Location = new System.Drawing.Point(122, 27);
+            this.numericUpDownProductPrice.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDownProductPrice.Name = "numericUpDownProductPrice";
+            this.numericUpDownProductPrice.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDownProductPrice.TabIndex = 5;
+            // 
             // CreateUpdateProductForm
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.numericUpDownProductPrice);
             this.Controls.Add(this.buttonCreateUpdateProduct);
-            this.Controls.Add(this.textBoxProductPrice);
             this.Controls.Add(this.textBoxProductName);
             this.Controls.Add(this.labelProductPrice);
             this.Controls.Add(this.labelProductName);
             this.Name = "CreateUpdateProductForm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownProductPrice)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
