2a3268ea096f28ac4938a12a8f51fe508cabb6eb
HanJuK <qwerty03081@gmail.com>
2022-06-11 00:45:44 +0900

    finished design for main screen and admin screen

--- windows-forms-project-assignment/AdminForm.cs
+++ windows-forms-project-assignment/AdminForm.cs
@@ -9,7 +9,11 @@ namespace windows_forms_project_assignment
         /** define constants */
         const string STRING_FORM_TITLE = "Self Ordering System - Admin Screen";
         private DataGridView dataGridViewProduct;
-        private Button buttonOrderHistory;
+        private Label labelProductListTitle;
+        private Label labelUpdateDeleteHint;
+        private Label labelOrderListTitle;
+        private DataGridView dataGridViewOrder;
+        private Label labelItemDetailHint;
 
         /** variables for child forms */
         private Button buttonCreateProduct;
@@ -23,56 +27,132 @@ namespace windows_forms_project_assignment
             this.InitializeComponent();
 
             this.refreshProductList();
+            this.initializeDataGridViewOrder();
         }
 
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.buttonCreateProduct = new System.Windows.Forms.Button();
             this.dataGridViewProduct = new System.Windows.Forms.DataGridView();
-            this.buttonOrderHistory = new System.Windows.Forms.Button();
+            this.labelProductListTitle = new System.Windows.Forms.Label();
+            this.labelUpdateDeleteHint = new System.Windows.Forms.Label();
+            this.labelOrderListTitle = new System.Windows.Forms.Label();
+            this.dataGridViewOrder = new System.Windows.Forms.DataGridView();
+            this.labelItemDetailHint = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonCreateProduct
             // 
-            this.buttonCreateProduct.Location = new System.Drawing.Point(395, 418);
+            this.buttonCreateProduct.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCreateProduct.Location = new System.Drawing.Point(368, 19);
             this.buttonCreateProduct.Name = "buttonCreateProduct";
-            this.buttonCreateProduct.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreateProduct.Size = new System.Drawing.Size(30, 27);
             this.buttonCreateProduct.TabIndex = 0;
-            this.buttonCreateProduct.Text = "Create Product";
+            this.buttonCreateProduct.Text = "+";
             this.buttonCreateProduct.Click += new System.EventHandler(this.buttonCreateProduct_Click);
             // 
             // dataGridViewProduct
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewProduct.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridViewProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewProduct.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewProduct.Location = new System.Drawing.Point(12, 52);
+            this.dataGridViewProduct.MultiSelect = false;
             this.dataGridViewProduct.Name = "dataGridViewProduct";
             this.dataGridViewProduct.ReadOnly = true;
             this.dataGridViewProduct.RowHeadersWidth = 51;
             this.dataGridViewProduct.RowTemplate.Height = 29;
-            this.dataGridViewProduct.Size = new System.Drawing.Size(458, 400);
+            this.dataGridViewProduct.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewProduct.Size = new System.Drawing.Size(386, 436);
             this.dataGridViewProduct.TabIndex = 1;
             this.dataGridViewProduct.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProduct_CellDoubleClick);
             // 
-            // buttonOrderHistory
+            // labelProductListTitle
             // 
-            this.buttonOrderHistory.Location = new System.Drawing.Point(504, 211);
-            this.buttonOrderHistory.Name = "buttonOrderHistory";
-            this.buttonOrderHistory.Size = new System.Drawing.Size(103, 23);
-            this.buttonOrderHistory.TabIndex = 2;
-            this.buttonOrderHistory.Text = "Order History";
-            this.buttonOrderHistory.UseVisualStyleBackColor = true;
-            this.buttonOrderHistory.Click += new System.EventHandler(this.buttonOrderHistory_Click);
+            this.labelProductListTitle.AutoSize = true;
+            this.labelProductListTitle.Font = new System.Drawing.Font("Malgun Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelProductListTitle.Location = new System.Drawing.Point(12, 12);
+            this.labelProductListTitle.Name = "labelProductListTitle";
+            this.labelProductListTitle.Size = new System.Drawing.Size(101, 30);
+            this.labelProductListTitle.TabIndex = 3;
+            this.labelProductListTitle.Text = "Products:";
+            // 
+            // labelUpdateDeleteHint
+            // 
+            this.labelUpdateDeleteHint.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.labelUpdateDeleteHint.Location = new System.Drawing.Point(12, 491);
+            this.labelUpdateDeleteHint.Name = "labelUpdateDeleteHint";
+            this.labelUpdateDeleteHint.Size = new System.Drawing.Size(386, 20);
+            this.labelUpdateDeleteHint.TabIndex = 4;
+            this.labelUpdateDeleteHint.Text = "Double-click an item to update/delete it.";
+            this.labelUpdateDeleteHint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelOrderListTitle
+            // 
+            this.labelOrderListTitle.AutoSize = true;
+            this.labelOrderListTitle.Font = new System.Drawing.Font("Malgun Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelOrderListTitle.Location = new System.Drawing.Point(417, 12);
+            this.labelOrderListTitle.Name = "labelOrderListTitle";
+            this.labelOrderListTitle.Size = new System.Drawing.Size(81, 30);
+            this.labelOrderListTitle.TabIndex = 5;
+            this.labelOrderListTitle.Text = "Orders:";
+            // 
+            // dataGridViewOrder
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewOrder.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewOrder.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrder.Location = new System.Drawing.Point(417, 52);
+            this.dataGridViewOrder.MultiSelect = false;
+            this.dataGridViewOrder.Name = "dataGridViewOrder";
+            this.dataGridViewOrder.ReadOnly = true;
+            this.dataGridViewOrder.RowTemplate.Height = 25;
+            this.dataGridViewOrder.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewOrder.Size = new System.Drawing.Size(457, 436);
+            this.dataGridViewOrder.TabIndex = 6;
+            this.dataGridViewOrder.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewOrder_CellDoubleClick);
+            // 
+            // labelItemDetailHint
+            // 
+            this.labelItemDetailHint.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.labelItemDetailHint.Location = new System.Drawing.Point(417, 491);
+            this.labelItemDetailHint.Name = "labelItemDetailHint";
+            this.labelItemDetailHint.Size = new System.Drawing.Size(457, 20);
+            this.labelItemDetailHint.TabIndex = 7;
+            this.labelItemDetailHint.Text = "Double-click an item to view it\'s details.";
+            this.labelItemDetailHint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // AdminForm
             // 
-            this.ClientSize = new System.Drawing.Size(648, 453);
-            this.Controls.Add(this.buttonOrderHistory);
+            this.ClientSize = new System.Drawing.Size(886, 520);
+            this.Controls.Add(this.labelItemDetailHint);
+            this.Controls.Add(this.dataGridViewOrder);
+            this.Controls.Add(this.labelOrderListTitle);
+            this.Controls.Add(this.labelUpdateDeleteHint);
+            this.Controls.Add(this.labelProductListTitle);
             this.Controls.Add(this.dataGridViewProduct);
             this.Controls.Add(this.buttonCreateProduct);
             this.Name = "AdminForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrder)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -92,6 +172,21 @@ namespace windows_forms_project_assignment
             dataGridViewProduct.DataSource = productsListWithoutDeleted;
 
             dataGridViewProduct.Columns["isDeleted"].Visible = false;
+
+            return;
+        }
+
+        private void initializeDataGridViewOrder()
+        {
+            dataGridViewOrder.DataSource = null;
+            dataGridViewOrder.DataSource = DataManager.orders;
+
+            dataGridViewOrder.Columns["cartDataInString"].Visible = false;
+
+            dataGridViewOrder.Columns["datetime"].DisplayIndex = 1;
+            dataGridViewOrder.Columns["phone"].DisplayIndex = 2;
+            dataGridViewOrder.Columns["grandTotal"].DisplayIndex = 3;
+            dataGridViewOrder.Columns["isMileageUsed"].DisplayIndex = 4;
 
             return;
         }
@@ -136,11 +231,16 @@ namespace windows_forms_project_assignment
             return;
         }
 
-        /** define EventHandlers */
-        private void buttonOrderHistory_Click(object sender, EventArgs e)
+        private void dataGridViewOrder_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            /** open the Order History Screen */
-            (new OrderHistoryForm()).ShowDialog();
+            /** handle exception for when clicked topmost row */
+            if (e.RowIndex == -1)
+            {
+                return;
+            }
+
+            /** open order history cart screen */
+            (new OrderHistoryCart(e.RowIndex)).ShowDialog();
 
             return;
         }
--- windows-forms-project-assignment/MainForm.cs
+++ windows-forms-project-assignment/MainForm.cs
@@ -10,6 +10,8 @@ namespace windows_forms_project_assignment
 
         /** variables for child forms */
         private Button buttonAdminForm;
+        private Label labelTitle;
+        private Label labelStudentInfo;
         private Button buttonOrderForm;
 
         /** MainForm class constructor (initialize) */
@@ -25,33 +27,57 @@ namespace windows_forms_project_assignment
         {
             this.buttonAdminForm = new System.Windows.Forms.Button();
             this.buttonOrderForm = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelStudentInfo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonAdminForm
             // 
-            this.buttonAdminForm.Location = new System.Drawing.Point(12, 12);
+            this.buttonAdminForm.Location = new System.Drawing.Point(32, 155);
             this.buttonAdminForm.Name = "buttonAdminForm";
-            this.buttonAdminForm.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdminForm.Size = new System.Drawing.Size(90, 45);
             this.buttonAdminForm.TabIndex = 0;
-            this.buttonAdminForm.Text = "Admin Form";
+            this.buttonAdminForm.Text = "Admin Mode";
             this.buttonAdminForm.Click += new System.EventHandler(this.buttonAdminForm_Click);
             // 
             // buttonOrderForm
             // 
-            this.buttonOrderForm.Location = new System.Drawing.Point(197, 12);
+            this.buttonOrderForm.Location = new System.Drawing.Point(162, 155);
             this.buttonOrderForm.Name = "buttonOrderForm";
-            this.buttonOrderForm.Size = new System.Drawing.Size(75, 23);
+            this.buttonOrderForm.Size = new System.Drawing.Size(90, 45);
             this.buttonOrderForm.TabIndex = 1;
-            this.buttonOrderForm.Text = "Order Form";
+            this.buttonOrderForm.Text = "Order Mode";
             this.buttonOrderForm.Click += new System.EventHandler(this.buttonOrderForm_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("Noto Sans KR Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTitle.Location = new System.Drawing.Point(12, 53);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(260, 47);
+            this.labelTitle.TabIndex = 2;
+            this.labelTitle.Text = "Self Ordering System";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelStudentInfo
+            // 
+            this.labelStudentInfo.AutoSize = true;
+            this.labelStudentInfo.Location = new System.Drawing.Point(169, 9);
+            this.labelStudentInfo.Name = "labelStudentInfo";
+            this.labelStudentInfo.Size = new System.Drawing.Size(103, 15);
+            this.labelStudentInfo.TabIndex = 3;
+            this.labelStudentInfo.Text = "18011567 김한주";
             // 
             // MainForm
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelStudentInfo);
+            this.Controls.Add(this.labelTitle);
             this.Controls.Add(this.buttonAdminForm);
             this.Controls.Add(this.buttonOrderForm);
             this.Name = "MainForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
